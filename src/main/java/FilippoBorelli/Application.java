0966cde3f41e12737c2c5ce30e95842cc683716d
Filippo2803 <filippoborelli8@gmail.com>
2024-03-22 11:29:44 +0100

    bases writed

--- src/main/java/FilippoBorelli/Application.java
+++ src/main/java/FilippoBorelli/Application.java
@@ -1,8 +1,15 @@
 package FilippoBorelli;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Scanner;
+
 public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("U4-W3-D5-Test");
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        EntityManager em = emf.createEntityManager();
     }
 }
